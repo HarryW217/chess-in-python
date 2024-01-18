502cce88560665f0ff79cf47927b4a94b5c60d5f
HarryW217 <harrywalker217@gmail.com>
2024-01-18 18:31:54 +0000

    Created tuple containing the RGB values for a dark_brown color, which is set at the background of my Chess board in the Main Game Loop section. The game loop controls the frame rate and the screen fill color while the game is running. For our event handling section, I have written a loop that iterates over the events that occur. If the user clicks the window's close button (represented by 'pygame.QUIT', we set run to False. flip() is used to update the screen at the end of each frame and quit() is used to uninitialised the game when the 'run' variable is set to False and the game loop exits.

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesandsbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
+
+# RGB values for dark brown
+
+dark_brown = (101, 67, 33)
+
+
+# Main Game Loop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill(dark_brown)
+    
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    pygame.display.flip()
+pygame.quit()
