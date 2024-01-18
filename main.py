f25ccbb9a57dc8150ece8317fbf0f70b32fdeef2
HarryW217 <harrywalker217@gmail.com>
2024-01-18 18:03:07 +0000

    Set up initial variables: width, height, screen, fonts, timer and frames per second

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#Chess in Python!
+import pygame
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+font = pygame.font.Font('freesansbold.ttf', 20)
+big_font = pygame.font.Font('freesandsbold.ttf', 50)
+timer = pygame.time.Clock()
+fps = 60
