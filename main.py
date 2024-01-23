774197c4eea6b91a976dc3c30e89439f0954ea13
HarryW217 <harrywalker217@gmail.com>
2024-01-23 19:35:21 +0000

    Assigned lists of white and black pieces and their coordinates on the Chess board to variables, as well as empty lists to variables for captured pieces to eventually be display on the screen alongside the board

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ big_font = pygame.font.Font(font_path, 50)
 timer = pygame.time.Clock()
 fps = 60
 
+# Game Variables and Images
+
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+white_locations = [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0),
+                   (0,1), (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,1)]
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+black_locations = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
+                   (0,6), (1,6), (2,6), (3,6), (4,6), (5,6), (6,6), (7,6)]
+captured_pieces_white = []
+captured_pieces_white = []
+
 # RGB values for dark brown
 
 dark_brown = (101, 67, 33)
