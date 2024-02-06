e74b1f05725c677d5121ede81ca2cf191a339a3a
HarryW217 <harrywalker217@gmail.com>
2024-02-06 14:46:02 +0000

    Created check pawn function which returns the valid moves for either a white or a black pawn, allowing them to move one square forward, two squares forward or a square diagonally to take an opponent's piece, relative to their position of the board

--- main.py
+++ main.py
@@ -135,24 +135,55 @@ def draw_pieces():
 def check_options(pieces, locations, turn):
     moves_list = []
     all_moves_list = []
-    for i in range((pieces)):
+    for i in range(len(pieces)):
         location = locations[i]
         piece = pieces[i]
         if piece == 'pawn':
             moves_list = check_pawn(location, turn)
-        elif piece == 'rook':
-            moves_list = check_rook(location, turn)
-        elif piece == 'knight':
-            moves_list = check_knight(location, turn)
-        elif piece == 'bishop':
-            moves_list = check_bishop(location, turn)
-        elif piece == 'queen':
-            moves_list = check_queen(location, turn)
-        elif piece == 'king':
-            moves_list = check_king(location, turn)
+        # elif piece == 'rook':
+        #     moves_list = check_rook(location, turn)
+        # elif piece == 'knight':
+        #     moves_list = check_knight(location, turn)
+        # elif piece == 'bishop':
+        #     moves_list = check_bishop(location, turn)
+        # elif piece == 'queen':
+        #     moves_list = check_queen(location, turn)
+        # elif piece == 'king':
+        #     moves_list = check_king(location, turn)
         all_moves_list.append(moves_list)
     return all_moves_list
 
+def check_pawn(position, color):
+    moves_list = []
+    if color == 'white':
+        if (position[0], position[1]+1) not in white_locations and \
+        (position[0], position[1]+1) not in black_locations and position[1]<7:
+            moves_list.append((position[0],position[1]+1))
+        if (position[0], position[1]+2) not in white_locations and \
+        (position[0], position[1]+2) not in black_locations and position[1]==1:
+            moves_list.append((position[0],position[1]+2))
+        if (position[0]+1, position[1]+1) not in white_locations and \
+        (position[0]+1, position[1]+1) not in black_locations and position[0]<7:
+            moves_list.append((position[0]+1, position[1]+1))
+        if (position[0]-1, position[1]+1) not in white_locations and \
+        (position[0]-1, position[1]+1) not in black_locations and position[0]>0:
+            moves_list.append((position[0]-1, position[1]+1))
+    else:
+        if (position[0], position[1]-1) not in white_locations and \
+        (position[0], position[1]-1) not in black_locations and position[1]>0:
+            moves_list.append((position[0],position[1]-1))
+        if (position[0], position[1]-2) not in white_locations and \
+        (position[0], position[1]-2) not in black_locations and position[1]==6:
+            moves_list.append((position[0],position[1]-2))
+        if (position[0]-1, position[1]-1) not in white_locations and \
+        (position[0]-1, position[1]-1) not in black_locations and position[0]>0:
+            moves_list.append((position[0]+1, position[1]+1))
+        if (position[0]+1, position[1]-1) not in white_locations and \
+        (position[0]+1, position[1]-1) not in black_locations and position[0]<7:
+            moves_list.append((position[0]+1, position[1]-1))
+    return moves_list
+            
+            
 
 # Main Game Loop
 black_options = check_options(black_pieces, black_locations, 'black') 
